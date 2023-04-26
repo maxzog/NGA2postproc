502d3173caf40d4deaceee3f1031c27c843bbb52
maxzog <maxzog@umich.edu>
2023-04-26 16:52:13 -0400

    Longitudinal and transverse decomposition implemented for tp_pic Lagrangian fluid velocity correlation

--- src/tp_pic.jl
+++ src/tp_pic.jl
@@ -1,4 +1,66 @@
 
+function pic_uu_lt(ps::Vector{part}, field::grid, rmax::Float64, nb::Int64; ncells=16)
+    re_id!(ps)
+    Δ = field.L / ncells
+    npic, ipic = part_grid(ps, Δ, ncells)
+    np = lastindex(ps)
+    @assert sum(npic) == np
+
+    no = floor(Int64, rmax/Δ)
+    if no == 0
+        rmax = norm([Δ, Δ, Δ])
+    else
+        rmax = (2*no+1)*norm([Δ, Δ, Δ])
+    end
+    dr = rmax/nb
+    uul = zeros(Float64, nb+1); rv = 0:dr:rmax
+    uut = zeros(Float64, nb+1); rv = 0:dr:rmax
+    c  = zeros(Int, nb+1); sc = 0; s = 0.
+
+    for p in ps
+        ip1, jp1, kp1 = get_ijk(p, ncells, Δ)
+        for i in ip1-no:ip1+no
+            for j in jp1-no:jp1+no
+                for k in kp1-no:kp1+no
+                    ii, jj, kk = periodic_inds([i,j,k], ncells)
+                    for ni in ipic[:,ii,jj,kk]
+                        if ni != 0 
+                            q = ps[ni]
+                            r = get_minr(p.pos, q.pos, field.L)
+                            ir = floor(Int, r/dr) + 1
+                            if p.id != q.id
+                                rl, rt = par_perp_u(p, q)
+                                uul[ir] += dot(p.fld+p.uf,rl)*dot(q.fld+q.uf,rl) 
+                                uut[ir] += dot(p.fld+p.uf,rt)*dot(q.fld+q.uf,rt) 
+                                c[ir]  += 1
+                            else
+                                uul[ir] += dot(p.fld+p.uf,q.fld+q.uf)
+                                uut[ir] += dot(p.fld+p.uf,q.fld+q.uf)
+                                c[ir] += 1
+                            end
+                        end
+                        if ni != 0 && ps[ni].id == p.id
+                            s += dot(p.fld+p.uf, ps[ni].fld + ps[ni].uf)
+                            sc += 1
+                        end
+                    end
+                end
+            end
+        end
+    end
+    c = [c[i]==0 ? c[i]=1 : c[i]=c[i] for i in 1:lastindex(c)]
+    uul = uul./(3*c); uut = uut./(3*c); s = s/(3*sc)
+    return dr, uul, uut, s
+end
+
+function par_perp_u(p::part, q::part)
+    r = q.pos-p.pos
+    rll = r/norm(r)
+    rt1 = cross(r,p.fld+p.uf) / norm(cross(r,p.fld+p.uf))
+    rt2 = cross(rt1,rll) / norm(cross(rt1,rll))
+    return rll, rt2
+end
+
 function pic_rdf(ps::Vector{part}, field::grid, rmax::Float64, nb::Int64; ncells=16)
     re_id!(ps)
     Δ = field.L / ncells
